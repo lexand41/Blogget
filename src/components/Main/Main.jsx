dfa1b6247450183e42ff00b2a16ebf1cd77e40fd
lexand41 <lexand41@yandex.ru>
2023-09-03 22:08:45 +0700

    React_lesson_03

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,9 @@
 import style from './Main.module.css';
 import Layout from '../Layout';
 
-export const Main = () => {
-  return (
-    <main className={style.main}>
-      <Layout>
+export const Main = () => (
+  <main className={style.main}>
+    <Layout>
 
-      </Layout>
-    </main>)
-}
+    </Layout>
+  </main>);
