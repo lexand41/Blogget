e1f5b47078f7da74ae5147e1129e34c6ed33bbb0
lexand41 <lexand41@yandex.ru>
2023-09-11 20:17:46 +0700

    module5_lesson6

---
+++ src/api/const.js
@@ -0,0 +1,9 @@
+export const URL_API = 'https://oauth.reddit.com';
+export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
+export const CLIENT_ID = 'pBasdcgp4FP3ErM7nlE48Q';
+export const RESPONSE_TYPE = 'token';
+export const RANDOM_STRING = 'random_string';
+export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const SCOPE = 'identity submit read';
+
+
--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,13 +1,73 @@
+import {useEffect, useState} from 'react';
 import style from './Auth.module.css';
 import PropTypes from 'prop-types';
-import {ReactComponent as Icon} from './img/login.svg';
+import {ReactComponent as LoginIcon} from './img/login.svg';
+import {Text} from '../../../UI/Text';
+import {urlAuth} from '../../../api/auth';
+import {URL_API} from '../../../api/const';
 
-export const Auth = ({auth}) => (
-  <button className={style.button}>
-    {auth ? auth : <Icon className={style.svg} />}
-  </button>
-);
+
+export const Auth = ({token, delToken}) => {
+  const [auth, setAuth] = useState({});
+  const [logout, setLogout] = useState(style.logout);
+
+  useEffect(() => {
+    if (!token) return;
+
+    fetch(`${URL_API}/api/v1/me`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    })
+      .then(response => {
+        console.log(response.status);
+        if (response.status === 401) {
+          localStorage.setItem('bearer', '');
+        }
+        return response;
+      })
+      .then(response => response.json())
+      .then(({name, icon_img: iconImg}) => {
+        const img = iconImg.replace(/\?.*$/, '');
+        setAuth({name, img});
+      })
+      .catch((err) => {
+        console.error(err);
+        setAuth({});
+      });
+  }, [token]);
+
+  const handleLogout = () => {
+    setLogout(style.active);
+  };
+
+  const handleCloseLogout = () => {
+    setLogout(style.logout);
+    delToken();
+  };
+
+  return (
+    <div className={style.container}>
+      {auth.name ? (
+        <button className={style.btn} onClick={handleLogout}>
+          <img
+            className={style.img}
+            src={auth.img}
+            title={auth.name}
+            alt={`Аватар ${auth.name}`}
+          />
+        </button>
+      ) : (
+        <Text className={style.authLink} As='a' href={urlAuth}>
+          <LoginIcon className={style.svg} />
+        </Text>
+      )}
+      <button className={logout} onClick={handleCloseLogout}>Выйти</button>
+    </div>
+  );
+};
 
 Auth.propTypes = {
-  auth: PropTypes.string,
+  token: PropTypes.string,
+  delToken: PropTypes.func,
 };
